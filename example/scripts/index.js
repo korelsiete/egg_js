d9846e39d6b2c5d0f2ec2c61bd0f4ebc4d282ee4
Stefano Flores <stefanoypf@gmail.com>
2024-04-12 14:24:22 -0500

    example - done

--- example/scripts/index.js
+++ example/scripts/index.js
@@ -3,3 +3,10 @@ const selector = document.getElementById("mensaje");
 
 /* escribir dentro de esa etiqueta con JS */
 selector.innerHTML = "CONEXIÓN EXITOSA ENTRE HTML Y JS";
+
+/* muestra un alerta que pide una entrada de dato */
+const namePrompt = globalThis.prompt("INGRESA TU NOMBRE:");
+const message = `Bienvenido ${namePrompt}`;
+
+/* muestra un alerta con el dato de entrada */
+alert(message);
