e40b4de11c3eb4e707b65b5fa11a56b68a3ac0f3
Stefano Flores <stefanoypf@gmail.com>
2024-04-17 13:56:35 -0500

    store - add functions

--- store/index.js
+++ store/index.js
@@ -1,23 +1,32 @@
-let totalAPagar = 0;
-let listProducts = "";
+const buy = () => {
+  let totalAPagar = 0;
+  let listProducts = "";
 
-const cantidad = Number(prompt("¿Cuántos productos desea llevar?"));
+  const cantidad = Number(prompt("¿Cuántos productos desea llevar?"));
 
-for (let index = 1; index <= cantidad; index++) {
-  const nombre = String(prompt(`${listProducts}¿Qué producto desea llevar?`));
-  const unidades = Number(
-    prompt(
-      `${listProducts}¿Cuántas unidades del producto "${nombre}" desea llevar?`
-    )
-  );
-  const precio = Number(
-    prompt(
-      `${listProducts}¿Cuánto cuesta cada unidad del producto "${nombre}"?`
-    )
-  );
-  const subtotal = unidades * precio;
-  listProducts += `Producto: "${nombre}" - Cantidad: ${unidades} - Precio: ${precio}\n`;
-  totalAPagar += subtotal;
-}
+  for (let index = 1; index <= cantidad; index++) {
+    const nombre = String(prompt(`${listProducts}¿Qué producto desea llevar?`));
+    const unidades = Number(
+      prompt(
+        `${listProducts}¿Cuántas unidades del producto "${nombre}" desea llevar?`
+      )
+    );
+    const precio = Number(
+      prompt(
+        `${listProducts}¿Cuánto cuesta cada unidad del producto "${nombre}"?`
+      )
+    );
+    const subtotal = unidades * precio;
+    listProducts += `Producto: "${nombre}" - Cantidad: ${unidades} - Precio: ${precio}\n`;
+    totalAPagar += subtotal;
+  }
 
-alert(`${listProducts}El total a pagar es: ${totalAPagar}`);
+  alert(`${listProducts}El total a pagar es: ${totalAPagar}`);
+  return totalAPagar;
+};
+
+const total1 = buy();
+const total2 = buy();
+const total3 = buy();
+const total = total1 + total2 + total3;
+console.log("El total de la compra es: " + total);
