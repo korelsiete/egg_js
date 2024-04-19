5326924a56cc62ecc23f86c60d636a90b5c77396
Stefano Flores <stefanoypf@gmail.com>
2024-04-18 19:59:09 -0500

    store - array activity3

--- store/products.js
+++ store/products.js
@@ -8,14 +8,26 @@ producto1.id = "id123";
 producto1["foto"] = "./images/celular.png";
 
 class Product {
-  constructor(id, title, price, stock, onSale, supplier) {
+  constructor(
+    id,
+    title,
+    price,
+    stock,
+    images,
+    onSale,
+    supplier,
+    colors = [],
+    description
+  ) {
     this.id = id;
     this.title = title;
     this.price = price;
     this.stock = stock;
-    this.images = [];
+    this.images = images;
     this.onSale = onSale;
     this._supplier = supplier;
+    this.colors = colors;
+    this.description = description;
   }
 
   get supplier() {
@@ -34,15 +46,51 @@ class Product {
   }
 }
 
-const prod1 = new Product(1, "Iphone", 500, 20, false, "Apple");
-const prod2 = new Product(2, "Samsung", 500, 15, true, "Samsung");
-const prod3 = new Product(3, "Xiaomi-Pro", 350, 10, true, "Xiaomi");
-const prod4 = new Product(4, "Huawei", 350, 12, true, "Huawei");
+const prod1 = new Product(
+  1,
+  "Iphone",
+  500,
+  20,
+  ["images/iphone1", "images/iphone2", "images/iphone3"],
+  false,
+  "Apple",
+  ["rojo", "verde"],
+  "descripción del producto"
+);
+const prod2 = new Product(
+  2,
+  "Samsung",
+  500,
+  15,
+  ["images/samsung1", "images/samsung2", "images/samsung3"],
+  true,
+  "Samsung",
+  ["azul", "plateado"],
+  "descripción del producto"
+);
+const prod3 = new Product(
+  3,
+  "Xiaomi-Pro",
+  350,
+  ["images/xiaomi1", "images/xiaomi2"],
+  10,
+  true,
+  "Xiaomi",
+  ["dorado", "negro"],
+  "descripción del producto"
+);
+const prod4 = new Product(
+  4,
+  "Huawei",
+  350,
+  12,
+  ["images/huawei1", "images/huawei2"],
+  true,
+  "Huawei",
+  ["morado", "negro"],
+  "descripción del producto"
+);
 
 const products = [prod1, prod2, prod3, prod4];
 
-console.log(products);
-products.shift();
-console.log(products);
-products.pop();
 console.log(products);
