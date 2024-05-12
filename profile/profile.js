4936654560dabc1b7d835bad07af3156acacbbe7
Stefano Flores <stefanoypf@gmail.com>
2024-05-12 00:46:19 -0500

    AND lógico - &&

--- profile/profile.js
+++ profile/profile.js
@@ -1,17 +1,23 @@
 const container = document.getElementById("container");
 
+const inputOnline = prompt("¿Está conectado el usuario?\nSI o NO", "SI");
+const isOnline = inputOnline?.toUpperCase() === "SI" ? true : false;
 const [nombre, edad, profesion, ubicacion, hobby] = [
   "Stefano",
   20,
-  "Software developer",
+  "Software Developer",
   "Lima",
   "Freestyle",
 ];
 
-const template = `Nombre: ${nombre}
+const template = `\n
+Nombre: ${nombre}
 Edad: ${edad}
 Profesión: ${profesion}
 Ubicación: ${ubicacion}
 Hobby: ${hobby}`;
 
-container.textContent = template;
+container.textContent = `Estado del usuario: ${
+  isOnline ? "Activo" : "Inactivo"
+}`;
+isOnline && (container.textContent += template);
